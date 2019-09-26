0c56cf21867e194319afc45c4161863876b6d67e
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2019-09-26 20:50:15 +0200

    rename api endpoint
    + more general naming and endpoint url
    
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- src/api-service/api_service/api.py
+++ src/api-service/api_service/api.py
@@ -2,8 +2,9 @@
 from flask import Flask
 from flask_restful import Resource, Api
 
+from api_service.utilities.api_url import story_url_for
 from api_service.resources.twentyfivestories import TwentyFiveStories
-from api_service.resources.tenkkarmastories import TenKKarmaStories
+from api_service.resources.tenkkarmastories import KarmaStories
 
 app = Flask(__name__)
 api = Api(app)
@@ -26,7 +27,7 @@ class SevenDaysStories(Resource):
 # Register API
 api.add_resource(TwentyFiveStories, "/twenty_five_stories")
 api.add_resource(SevenDaysStories, "/seven_days_stories")
-api.add_resource(TenKKarmaStories, "/ten_k_karma_stories")
+api.add_resource(KarmaStories, "/karma_stories")
 
 if __name__ == "__main__":
     app.run(debug=True)
