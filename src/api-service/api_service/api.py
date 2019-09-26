83a2afdbc0bfe1404b294cc8369159eb558c11ab
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2019-09-26 13:30:06 +0200

    add twentyFiveStories resource
    + query 25 stories extract the title and concat the titles on a string variable
    + return titles string variable
    
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- src/api-service/api_service/api.py
+++ src/api-service/api_service/api.py
@@ -1,6 +1,8 @@
 """API Service."""
+import requests
 from flask import Flask
 from flask_restful import Resource, Api
+from requests_futures.sessions import FuturesSession
 
 app = Flask(__name__)
 api = Api(app)
@@ -23,6 +25,7 @@ def story_url_for(idx=None):
 
     return f"{BASE_URL}/v{API_V}/item/{idx}.json"
 
+
 # Resources
 class TwentyFiveStories(Resource):
     """25 Stories API Endpoint."""
@@ -31,7 +34,31 @@ class TwentyFiveStories(Resource):
         Get-Method.
         :return:
         """
-        return True
+        titles = ""  # string representation of all titles
+        # counts number of stories where the title can be extracted
+        story_counter = 0
+
+        new_stories_top500 = requests.get(url=newstories_url()).json()
+
+        # reduce list to maximum stories required
+        stories = new_stories_top500[:MAX_STORIES]
+
+        # Query the 25th latest stories asynchronously by using their id from the
+        # previous query.
+        session = FuturesSession()
+
+        res_stories = [session.get(story_url_for(idx=story_id)) for story_id in stories]
+        for story in res_stories:
+            content = story.result().json()
+
+            # Validates the content to be existent and verify that the title key exists
+            # Deleted stories are flagged with {deleted:True} and do not contain a
+            # title key
+            if content is not None and "title" in content:
+                titles += f" {content['title']}"
+                story_counter += 1
+
+        return titles
 
 
 class SevenDaysStories(Resource):
