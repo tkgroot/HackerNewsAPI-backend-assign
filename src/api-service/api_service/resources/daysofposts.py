a2b44018d44346aeb8042b91bb40a0ada416d534
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2019-09-29 01:22:43 +0200

    disable query return static params
    + transform token to lowercase
    
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- src/api-service/api_service/resources/daysofposts.py
+++ src/api-service/api_service/resources/daysofposts.py
@@ -32,49 +32,60 @@ class DaysOfPosts(Resource):
 
         :return: {dict} - 10 most frequent used words in given time frame
         """
-        titles = ""  # string representation of the titles
-        item = {"id": requests.get(maxitem_url()).json()}  # set last item id from HN
+        # titles = ""  # string representation of the titles
+        # item = {"id": requests.get(maxitem_url()).json()}  # set last item id from HN
+        #
+        # now = datetime.datetime.now().replace(microsecond=0)  # set current time
+        # # set the lower and upper bound timestamp for time frame search
+        # # add the utc timezone per default and format in unix timestamp
+        # lb_timestamp = datetime.datetime(
+        #     now.year,
+        #     now.month,
+        #     now.day - DAY_DIFF,
+        #     second=SECONDS_LOWER,
+        #     tzinfo=datetime.timezone.utc,
+        # ).strftime("%s")
+        # ub_timestamp = datetime.datetime(
+        #     now.year,
+        #     now.month,
+        #     now.day - DAY_DIFF,
+        #     hour=HOURS_UPPER,
+        #     minute=MINUTES_UPPER,
+        #     second=SECONDS_UPPER,
+        #     tzinfo=datetime.timezone.utc,
+        # ).strftime("%s")
+        #
+        # # find the upper and lower bound item from HN API
+        # upperbound_item = find_story_time_boundary(ub_timestamp, item)
+        # lowerbound_item = find_story_time_boundary(
+        #     lb_timestamp, upperbound_item, interval=LOWER_BOUND_INTERVAL, reverse=True
+        # )
+        #
+        # # create HN item id array from lower bound to upper bound, step size 1
+        # stories_in_boundary = np.arange(lowerbound_item["id"], upperbound_item["id"], 1)
+        #
+        # # create query session and query the stories in stories_in_boundary
+        # session = FuturesSession()
+        # res_stories = [
+        #     session.get(url=story_url_for(story_id), hooks={"response": get_title_hook})
+        #     for story_id in stories_in_boundary
+        # ]
+        #
+        # # add new found stories titles and append them to titles
+        # for story in res_stories:
+        #     story = story.result()
+        #     if story.data is not None:
+        #         titles += f" {story.data}"
 
-        now = datetime.datetime.now().replace(microsecond=0)  # set current time
-        # set the lower and upper bound timestamp for time frame search
-        # add the utc timezone per default and format in unix timestamp
-        lb_timestamp = datetime.datetime(
-            now.year,
-            now.month,
-            now.day - DAY_DIFF,
-            second=SECONDS_LOWER,
-            tzinfo=datetime.timezone.utc,
-        ).strftime("%s")
-        ub_timestamp = datetime.datetime(
-            now.year,
-            now.month,
-            now.day - DAY_DIFF,
-            hour=HOURS_UPPER,
-            minute=MINUTES_UPPER,
-            second=SECONDS_UPPER,
-            tzinfo=datetime.timezone.utc,
-        ).strftime("%s")
-
-        # find the upper and lower bound item from HN API
-        upperbound_item = find_story_time_boundary(ub_timestamp, item)
-        lowerbound_item = find_story_time_boundary(
-            lb_timestamp, upperbound_item, interval=LOWER_BOUND_INTERVAL, reverse=True
-        )
-
-        # create HN item id array from lower bound to upper bound, step size 1
-        stories_in_boundary = np.arange(lowerbound_item["id"], upperbound_item["id"], 1)
-
-        # create query session and query the stories in stories_in_boundary
-        session = FuturesSession()
-        res_stories = [
-            session.get(url=story_url_for(story_id), hooks={"response": get_title_hook})
-            for story_id in stories_in_boundary
+        return [
+            ["hn", 58],
+            ["show", 34],
+            ["'s", 32],
+            ["ask", 23],
+            ["google", 16],
+            ["2019", 10],
+            ["new", 9],
+            ["world", 9],
+            ["13", 9],
+            ["software", 8],
         ]
-
-        # add new found stories titles and append them to titles
-        for story in res_stories:
-            story = story.result()
-            if story.data is not None:
-                titles += f" {story.data}"
-
-        return top_ten_words(titles)
--- src/api-service/api_service/resources/karmastories.py
+++ src/api-service/api_service/resources/karmastories.py
@@ -22,64 +22,75 @@ class KarmaStories(Resource):
 
         :return: {dict} - 10 most frequent used words
         """
-        titles = ""  # string representation of all titles
-        additional_stories = 0  # counts the additional stories found during API query
+        # titles = ""  # string representation of all titles
+        # additional_stories = 0  # counts the additional stories found during API query
+        #
+        # stories = requests.get(url=newstories_url()).json()  # [21076026]
+        # last_story_id = stories[-1:][0]
+        #
+        # # --* Create asynchronous sessions *--
+        # # others - handles the amount stories which are not contained in the
+        # # 'newstories' HN API query
+        # # sessions - handles the stories which are contained in the
+        # # 'newstories' HN API query
+        # others = FuturesSession()
+        # session = FuturesSession()
+        #
+        # # prevents limit to be a negative number
+        # limit = (MAX_STORIES - len(stories)) if (MAX_STORIES - len(stories)) > 0 else 0
+        #
+        # # run until limit of stories is reached
+        # while additional_stories < limit:
+        #     stories_left = limit - additional_stories
+        #
+        #     # array should start with the previous to last story and be of the size of
+        #     # the amount of stories left to reach the given limit
+        #     # previous to last story available => (last_story_id - 1)
+        #     story_id_array = np.arange(
+        #         last_story_id - 1, (last_story_id - 1) - stories_left, -1
+        #     )
+        #
+        #     result = [
+        #         others.get(
+        #             url=story_url_for(story_id),
+        #             hooks={"response": find_story_title_for_karma_hook},
+        #         )
+        #         for story_id in story_id_array
+        #     ]
+        #
+        #     # add new found stories
+        #     for story in result:
+        #         story = story.result()
+        #         if story.data is not None:
+        #             # title of the story or None if user doesn't have enough Karma
+        #             titles += f" {story.data}"
+        #             # while-loop-anker - increase by one since one story is found
+        #             additional_stories += 1
+        #
+        #     last_story_id = story_id_array[-1:][0]
+        #
+        # res_stories = [
+        #     session.get(
+        #         url=story_url_for(story_id),
+        #         hooks={"response": find_story_title_user_karma_hook},
+        #     )
+        #     for story_id in stories
+        # ]
+        #
+        # # add new found stories
+        # for story in res_stories:
+        #     story = story.result()
+        #     titles += f" {story.data}"
 
-        stories = requests.get(url=newstories_url()).json()  # [21076026]
-        last_story_id = stories[-1:][0]
-
-        # --* Create asynchronous sessions *--
-        # others - handles the amount stories which are not contained in the
-        # 'newstories' HN API query
-        # sessions - handles the stories which are contained in the
-        # 'newstories' HN API query
-        others = FuturesSession()
-        session = FuturesSession()
-
-        # prevents limit to be a negative number
-        limit = (MAX_STORIES - len(stories)) if (MAX_STORIES - len(stories)) > 0 else 0
-
-        # run until limit of stories is reached
-        while additional_stories < limit:
-            stories_left = limit - additional_stories
-
-            # array should start with the previous to last story and be of the size of
-            # the amount of stories left to reach the given limit
-            # previous to last story available => (last_story_id - 1)
-            story_id_array = np.arange(
-                last_story_id - 1, (last_story_id - 1) - stories_left, -1
-            )
-
-            result = [
-                others.get(
-                    url=story_url_for(story_id),
-                    hooks={"response": find_story_title_for_karma_hook},
-                )
-                for story_id in story_id_array
-            ]
-
-            # add new found stories
-            for story in result:
-                story = story.result()
-                if story.data is not None:
-                    # title of the story or None if user doesn't have enough Karma
-                    titles += f" {story.data}"
-                    # while-loop-anker - increase by one since one story is found
-                    additional_stories += 1
-
-            last_story_id = story_id_array[-1:][0]
-
-        res_stories = [
-            session.get(
-                url=story_url_for(story_id),
-                hooks={"response": find_story_title_user_karma_hook},
-            )
-            for story_id in stories
+        return [
+            ["'s", 20],
+            ["new", 11],
+            ["hn", 11],
+            ["ask", 8],
+            ["pdf", 7],
+            ["google", 7],
+            ["2019", 5],
+            ["china", 5],
+            ["quantum", 5],
+            ["device", 5],
         ]
-
-        # add new found stories
-        for story in res_stories:
-            story = story.result()
-            titles += f" {story.data}"
-
-        return top_ten_words(titles)
