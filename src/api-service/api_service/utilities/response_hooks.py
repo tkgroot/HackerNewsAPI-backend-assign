a5b76f3365c601e5d56276c3c9b6ac75f470bf5c
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2019-09-28 15:24:46 +0200

    create get_title_hook
    + returns the title of the item in question. Item can also be sth else the just a story
    
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- src/api-service/api_service/utilities/response_hooks.py
+++ src/api-service/api_service/utilities/response_hooks.py
@@ -5,6 +5,8 @@ from api_service.utilities.api_url import find_user_by
 
 # TODO: use .env for variable declaration instead
 KARMA_LIMIT = 1000  # contains a constant for the karma limit
+# contains the type of stories which titles are allowed to be processed
+ALLOWED_STORY_TYPES = "story"
 
 
 def find_story_title_user_karma_hook(resp, *args, **kwargs):
@@ -46,3 +48,27 @@ def find_story_title_for_karma_hook(resp, *args, **kwargs):
         find_story_title_user_karma_hook(resp=resp)
     else:
         resp.data = None
+
+
+def get_title_hook(resp, *args, **kwargs):
+    """
+
+    :param resp:
+    :param args:
+    :param kwargs:
+    :return:
+    """
+    resp.data = None  # initialize data attribute for response object resp
+    data = resp.json()  # retrieve response json from response object
+
+    # There might be items with an id but no content FTW!
+    if data is None or "deleted" in data:
+        resp.data = None
+    else:
+        # TODO: add kwargs handler to overwrite the ALLOWED_STORY_TYPES manually
+        #  maybe if one just want to get_titles_hook for only {type: story} only
+        #  kwargs["only"] = ("story")
+        if data["type"] in ALLOWED_STORY_TYPES:
+            # double check if item actually contains a title field
+            # print(resp.data["title"])
+            resp.data = data["title"] if "title" in data else None
