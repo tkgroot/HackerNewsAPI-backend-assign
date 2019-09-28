3e63082db43e37041959666df952b86e74311fd9
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2019-09-28 20:15:21 +0200

    change flask instance creation
    + create app.py to run flask_app
    + remove api.py
    + add api.py content to __init__
    + add create_app for flask app creation
    
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- src/api-service/api_service/__init__.py
+++ src/api-service/api_service/__init__.py
@@ -1,2 +1,22 @@
 """API-Service."""
 __version__ = "0.1.0"
+
+from flask import Flask
+from flask_restful import Api
+
+from api_service.resources.twentyfivestories import TwentyFiveStories
+from api_service.resources.karmastories import KarmaStories
+from api_service.resources.daysofposts import DaysOfPosts
+
+
+def create_app():
+    """Create instance of the FLASK application."""
+    app = Flask(__name__)
+    api = Api(app)
+
+    # Register API
+    api.add_resource(TwentyFiveStories, "/twenty_five_stories")
+    api.add_resource(DaysOfPosts, "/days_of_posts")
+    api.add_resource(KarmaStories, "/karma_stories")
+
+    return app
---
+++ src/api-service/app.py
@@ -0,0 +1,4 @@
+"""FLASK INSTANCE."""
+from api_service import create_app
+
+app = create_app()
